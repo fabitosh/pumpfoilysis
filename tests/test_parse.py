91d69f5fb63f3eff48acfed01db56c3bad0cfede
Fabio Meier <fabitosh@gmail.com>
2025-09-19 12:00:13 +0200

    refactor(tests): introduce tests/samples/, clean up AI crap

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -4,26 +4,17 @@ from pumpfoilysis.schemas import SCHEMA_RAW
 
 
 def test_parse_tcx():
-    """Tests the parse_tcx function."""
-    # Given
-    file_path = "/Users/fabio/repos/github/fabitosh/pumpfoilysis/tests/cut_activity.tcx"
-
-    # When
+    file_path = "/samples/cut_activity.tcx"
     df = parse_tcx(file_path)
-
-    # Then
     assert isinstance(df, pl.DataFrame)
     assert not df.is_empty()
 
-    # Infer required columns from SCHEMA_RAW, but rename timestamp to datetime_raw
+    # Minimum Set defined in Schema that will be used subsequently
     expected_columns = list(SCHEMA_RAW.keys())
-
-    # Also check for the other columns that are not in SCHEMA_RAW
+    # Optional columns that are present in Forerunner 945 LTE recordings
     expected_columns.extend(["distance_raw", "heart_rate_raw", "speed_raw"])
-
     assert all(col in df.columns for col in expected_columns)
 
-    # Check dtypes
     assert df["datetime"].dtype == pl.Datetime(time_unit="us", time_zone="UTC")
     assert df["lat_raw"].dtype == pl.Float64
     assert df["lon_raw"].dtype == pl.Float64
