dbec9de48064bca8790c39a744309bc583ebdc3d
Fabio Meier <fabitosh@gmail.com>
2025-09-20 21:33:05 +0200

    feat(main): minimum pipeline

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from pathlib import Path
+
+from pumpfoilysis.parse import parse_tcx
+from pumpfoilysis.refine import calc_refine_features
+
+
 def main():
-    print("Hello from pumpfoilysis!")
+    ACTIVITY_PATH = Path("data/activity_20238901238.tcx")
+    df_raw = parse_tcx(ACTIVITY_PATH)
+    df_refined = calc_refine_features(df_raw)
 
 
 if __name__ == "__main__":
