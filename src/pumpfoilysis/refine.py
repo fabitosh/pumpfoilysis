3186ceea3f00793a225fb07ede27bf2a48977fd7
Fabio Meier <fabitosh@gmail.com>
2025-09-20 15:49:52 +0200

    feat(refine): handle gps gaps, privatize subroutines

--- src/pumpfoilysis/refine.py
+++ src/pumpfoilysis/refine.py
@@ -11,6 +11,47 @@ def linearize_geo(
     Flat Earth Linearization around the reference coordinate.
     Inaccurate for about 0.5% for distances ~20km. This might be an issue with downwind pumping.
 
+
+def calc_refine_features(df: pl.DataFrame) -> pl.DataFrame:
+    cols_gps = ["lat_raw", "lon_raw"]
+    df_idx = _calc_generic_features(df.drop(cols_gps))
+    df_gps = df.filter(
+        pl.col("lat_raw").is_not_null()
+        & pl.col("lon_raw").is_not_null()
+    ).select(["datetime"] + cols_gps)
+    return df_idx.join(_calc_gps_features(df_gps), on="datetime", how="left")
+
+
+def _calc_generic_features(df: pl.DataFrame) -> pl.DataFrame:
+    return df.with_columns(
+        sampling_rate=pl.col("datetime").diff(),
+        # delta_distance_raw=pl.col("distance_raw").diff(),  # todo: remove later
+    )
+
+def _calc_gps_features(df: pl.DataFrame) -> pl.DataFrame:
+    """Expects a dataframe with nan-filtered gps values."""
+    # assert df.select("lat_raw", "lon_raw", "x", "y").null_count().sum() > 0, "GPS DataFrame contains null values."
+    df = _linearize_geo(df)
+    return df.with_columns(
+        gps_sampling_rate=pl.col("datetime").diff(),
+        delta_x=pl.col("x").diff(),
+        delta_y=pl.col("y").diff(),
+    ).with_columns(
+        heading=(pl.arctan2(pl.col("delta_y"), pl.col("delta_x"))).degrees(),
+        distance=(pl.col("delta_x") ** 2 + pl.col("delta_y") ** 2) ** 0.5,
+    ).with_columns(
+        speed_kmh=(pl.col("distance") / pl.col("gps_sampling_rate").dt.total_seconds() * 3.6)
+        .fill_null(0)
+        .alias("speed_kmh")
+    )
+
+def _linearize_geo(
+        df: pl.DataFrame, linearization_coordinate: tuple[float, float] | None = None
+) -> pl.DataFrame:
+    """
+    Flat Earth Linearization around the reference coordinate.
+    Inaccurate for about 0.5% for distances ~20km. This might be an issue with downwind pumping.
+
     The motivation is to be able to work with intuitive units instead of the ~4th decimal of the coordinate.
 
     Besides, filters would have different behaviors depending on the latitude of the tracking.
@@ -26,7 +67,7 @@ def linearize_geo(
     lat_ref, lon_ref = map(math.radians, linearization_coordinate)
     return df.with_columns(
         x=pl.lit(RADIUS_EARTH)
-        * (pl.col("lon_raw").radians() - lon_ref)
-        * math.cos(lat_ref),
+          * (pl.col("lon_raw").radians() - lon_ref)
+          * math.cos(lat_ref),
         y=pl.lit(RADIUS_EARTH) * (pl.col("lat_raw").radians() - lat_ref),
     )
